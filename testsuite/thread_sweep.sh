12121118892f8f3fde862bdc67cc6436624c95f6
nicholaiTukanov <nicholaitukanov@gmail.com>
2021-07-14 21:27:26 +0000

    more results

---
+++ testsuite/thread_sweep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 5th - BLIS_JC_NT (n)
+# 3rd - BLIS_IC_NT (m)
+# 2nd - BLIS_JR_NT (n)
+# 1st - BLIS_IR_NT (m)
+
+for j in 2 4 8 16 32
+do
+    ((i=64/$j))
+    export BLIS_IC_NT=${j}
+    export BLIS_JR_NT=${i}
+    ./test_libblis.x > test_IC_${j}_JR_${i}
+done
